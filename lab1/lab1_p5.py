72611937a644ef3200bd2dd29e17862f6f9170bd
Danny-IV <dh46656@gmail.com>
2025-03-31 10:27:37 +0900

    Fix: lab1_p5 formatting

--- lab1/lab1_p5.py
+++ lab1/lab1_p5.py
@@ -7,7 +7,7 @@ jpeg = 800 * 600 * 3 // 25
 png = 800 * 600 * 3 // 8
 tiff = 800 * 600 * 6
 
-print(format(size//gif, "6") + " image(s) in GIF format can be stored")
-print(format(size//jpeg, "6") + " image(s) in JPEG format can be stored")
-print(format(size//png, "6") + " image(s) in PNG format can be stored")
-print(format(size//tiff, "6") + " image(s) in TIFF format can be stored")
+print(format(size // gif, "6") + " image(s) in GIF format can be stored")
+print(format(size // jpeg, "6") + " image(s) in JPEG format can be stored")
+print(format(size // png, "6") + " image(s) in PNG format can be stored")
+print(format(size // tiff, "6") + " image(s) in TIFF format can be stored")
